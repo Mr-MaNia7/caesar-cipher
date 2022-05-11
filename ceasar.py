be46ec0576f7ff2c7b06565d8e3a926cad11dea5
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-05-11 10:49:50 +0300

    Add some docs, correct typos

--- ceasar.py
+++ ceasar.py
@@ -1,23 +1,40 @@
-import string
+"""Caesar Cipher
 
-"""Ceasar Cipher
+A program for demonstrating caesar cipher!
 
-A program for demonstrating ceasar cipher!
+This application allows the user to encrypt alphabetic text
+and see the encrypted text in real time.
+
+The app accepts user input through a terminal and it expects
+the user to input only alphabetic characters. Otherwise the 
+charaters are avoided and hence will not be encrypted!
+
+This program contains the following functions:
+    * caesarEnc - encrypts a char shifting it with a key
+    * main - the main function of the program
 """
 
-def ceasarEnc(char, key):
-    """An encrypting function accepting characters and returning swiched ones."""
+# Import necessary modules
+import string
+
+def caesarEnc(char, key):
+    """An encrypting function implementing caesar cipher."""
     ascii_number = ord(char)
-    
-    if (65 <= ascii_number <= 90):
+    skip_number = [10] # tab character
+    if ascii_number in skip_number:
+        return chr(ascii_number)
+    elif (65 <= ascii_number <= 90):
         num = ascii_number - 65
         enc_pos = (num + key) % 26 + 65
+        return chr(enc_pos)
     elif (97 <= ascii_number <= 122):
         num = ascii_number - 97
         enc_pos = (num + key) % 26 + 97
-    return chr(enc_pos)
+        return chr(enc_pos)
 
 def main():
+    """The main entry of the program."""
+    
     plain_txt = input("Enter a plain text: ")
     key = int(input("Enter a shift key: "))
     
@@ -28,7 +45,7 @@ def main():
         temp_list=[]
         word = word.translate(str.maketrans("", "", string.punctuation))
         for char in word:
-            temp_list.append(ceasarEnc(char, key))
+            temp_list.append(caesarEnc(char, key))
         temp_list.append(" ") # undoing the space removal
         enc_list.extend(temp_list)
     
